fcffe4904fdea17057ab0ba6b78d5f8918f389b9
mrajeswarasai <50444037+mrajeswarasai@users.noreply.github.com>
2020-12-02 21:02:02 +0530

    Pre-Heroku-Deploy

--- tnc/tnc/settings.py
+++ tnc/tnc/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMP_DIR=os.path.join(BASE_DIR,'templates')
@@ -126,3 +126,7 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
